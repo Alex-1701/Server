b08cb9228711af0662190dd4bd2a06030e33e865
Alex-1701 <55504833+Alex-1701@users.noreply.github.com>
2021-05-21 19:39:42 +0300

    2

--- part.js
+++ part.js
@@ -1,16 +1,46 @@
+const { json } = require('body-parser')
+
 const sqlite3 = require('../../node_modules/sqlite3').verbose()
 
 function getJournal(status, id, option, value) {
-  db = openDB()
-	console.log("lol")
+  // Запрос к журналу может выполнять студент, тогда он получит журнал своей группы.
+  // Или любой другой пользователь, тогда он должен указать группу.
+
+  let db = openDB()
+  var group
+  if (status == "Student"){
+    sql = `SELECT "group" FROM Student WHERE student_id = ${id}`
+    
+    db.get(sql, (err, row) => {
+      if (err) {
+        console.log("database error")
+        console.log(err.message);
+      }
+      else{
+        if(row){
+          console.log(row)
+          group = parseInt(row.group)
+        }
+        else{
+          console.log('No students found');
+        }
+      }
+    })
+  }
+  else {
+    // Тут нужно подумать
+  }
+  
+
+	
+  
+  console.log("lol")
 	console.log(option)
   console.log(value)
-  
+  console.log(group)
+
 	return "kek"
 }
-
-module.exports.getJournal = getJournal
-
 
 function openDB() {
   let db = new sqlite3.Database('dataBase.db', sqlite3.OPEN_READWRITE, (err) => {
@@ -22,3 +52,5 @@ function openDB() {
   })
   return db
 }
+
+module.exports.getJournal = getJournal
